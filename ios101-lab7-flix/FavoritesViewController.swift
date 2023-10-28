5ed2b37b1eb049f91fd52ac1450f185677fc377e
Yasaman Emami <emami.yasamann@gmail.com>
2023-10-28 12:12:05 -0700

    all

--- ios101-lab7-flix/FavoritesViewController.swift
+++ ios101-lab7-flix/FavoritesViewController.swift
@@ -22,7 +22,12 @@ class FavoritesViewController: UIViewController, UITableViewDataSource {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
+        // 1.
+        let movies = Movie.getMovies(forKey: Movie.favoritesKey)
+        // 2.
+        self.favoriteMovies = movies
+        // 3.
+        tableView.reloadData()
         // Anything in the defer call is guaranteed to happen last
         defer {
             // Show the "Empty Favorites" label if there are no favorite movies
